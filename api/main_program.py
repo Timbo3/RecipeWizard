c07e3e599ce071964259ddc5f12f579b15ba6ff9
Tim M <timjmartin475@gmail.com>
2021-12-28 20:17:51 +0000

    test

--- api/main_program.py
+++ api/main_program.py
@@ -1,24 +1,77 @@
 
 from fastapi import FastAPI, Path
 from fastapi.middleware.cors import CORSMiddleware
-from recipe_list import FindRecipesByIngredientMatches
-from ingredient_information import GetValidIngredientList
-from typing import List
 from pydantic import BaseModel
+import mysql.connector
+from database_connnection_details import config
+import time
+from collections import Counter
+
+def GetValidIngredientList():
+
+    db = mysql.connector.connect(**config)
+    cursor = db.cursor()
+    start_time = time.time()
+    sqlquery = "select name from ingredient"
+    cursor.execute(sqlquery)        
+    ListOfValidIngredientNames = cursor.fetchall()
+    print ("Getting list of all different ingredients from database took ", time.time() - start_time," seconds to run")
+    return (ListOfValidIngredientNames)
+
+
+
+def FindRecipesByIngredientMatches(UsersIngredientList, MaximumIngredientsInRecipes):
+    db = mysql.connector.connect(**config)
+    cursor = db.cursor()
+    start_time = time.time()
+    UsersIngredientList= UsersIngredientList.split(",")
+    recipeidlist=()
+    counter=0
+    while counter<len(UsersIngredientList):
+        cursor.execute("select recipe_ids from ingredient where name = '"+UsersIngredientList[counter]+"'")        
+        recipeidlist = sum((recipeidlist, cursor.fetchone()), ())
+        counter+=1
+    cursor.close()
+    recipeidlist = ','.join(recipeidlist)
+    recipeidlist = list(recipeidlist.split(",")) 
+    # print ("recipeidlist is ", recipeidlist)
+    print ("Getting a list of recipe id's from database for users ingredients took ", time.time() - start_time," seconds to run")
+    start_time = time.time()
+    CleanedandSortedRecipeIDList = [item for items, c in Counter(recipeidlist).most_common()
+    for item in [items] * c]
+    CleanedandSortedRecipeIDList = list(dict.fromkeys(CleanedandSortedRecipeIDList))
+    CleanedandSortedRecipeIDListLength = len(CleanedandSortedRecipeIDList)
+    if CleanedandSortedRecipeIDListLength>500:
+        print ("Shortening List to 500 items..")
+        CleanedandSortedRecipeIDList= CleanedandSortedRecipeIDList[0:500]       
+    print ("Length of recipe id list is ",len(CleanedandSortedRecipeIDList))
+    print ("Sorting recipe id list, removing duplicates, limiting to 500 id's the took ", time.time() - start_time," seconds to run")
+    start_time = time.time()
+    CleanedandSortedRecipeIDList= ','.join(CleanedandSortedRecipeIDList)
+    sqlquery = "select id,name,ingredient_list,servings,method,picture_url from recipe where id IN ("+CleanedandSortedRecipeIDList+") AND (recipe.different_ingredients <="+MaximumIngredientsInRecipes+")"
+    CleanedandSortedRecipeIDListWithoutSpeechMarks = CleanedandSortedRecipeIDList.replace("'","")
+    sqlquery = sqlquery + " ORDER BY FIND_IN_SET(id,'"+CleanedandSortedRecipeIDListWithoutSpeechMarks+"'), picture_url ASC"
+    print ("Querying database recipe table for recipes..")
+    #print ("SQL Query is ", sqlquery)
+    cursor = db.cursor()
+    cursor.execute(sqlquery)        
+    print ("Loading results from database into list.")
+    RecipeResultList = cursor.fetchall()
+    print ("Querying the database with the recipe id list took ", time.time() - start_time," seconds to run")
+    cursor.close()
+    #print (RecipeResultList)
+    return (RecipeResultList)
+
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
 
-app.add_middleware(
-     CORSMiddleware,
-     allow_origins=["*"],
-     allow_credentials=True,
-     allow_methods=["*"],
-     allow_headers=["*"]
-)
+
 
 @app.get("/valid_ingredient_list/")
 def search_by_ingredients_api_request():
      return {"Ingredients": GetValidIngredientList()}
+
 
 @app.get("/search_recipes_by_ingredients/{user_ingredient_list}/{maximum_ingredients_for_recipes}")
 def search_by_ingredients_api_request(user_ingredient_list:str = Path(None, description = "Comma seperated list of valid ingredient names"), 
@@ -28,12 +81,14 @@ maximum_ingredients_for_recipes:str = Path(None, description = "Integer value fo
           ID: str
           Title: str
           Ingredients: str
-          URL: str
+          Servings: str
+          Method: str
           Picture_URL: str
 
+
      db_tuple_with_recipe_results = FindRecipesByIngredientMatches(user_ingredient_list, maximum_ingredients_for_recipes)
-     recipes = [Recipe(ID=ID, Title=Title, Ingredients=Ingredients, URL=URL, Picture_URL=Picture_URL) 
-     for ID, Title, Ingredients,URL,Picture_URL in db_tuple_with_recipe_results]
+     recipes = [Recipe(ID=ID, Title=Title, Ingredients=Ingredients, Servings=Servings, Method=Method, Picture_URL=Picture_URL) 
+     for ID, Title, Ingredients, Servings, Method, Picture_URL in db_tuple_with_recipe_results]
      return {"Recipes": recipes}
 
 
--- api/recipewizard_test.py
+++ api/recipewizard_test.py
@@ -1,10 +1,11 @@
-from recipe_list import FindRecipesByIngredientMatches
+from main_program import FindRecipesByIngredientMatches
+from main_program import GetValidIngredientList
 from main_program import app
 import requests
 
 
 def test_get_valid_ingredient_api_response() -> None:
-    response = requests.get('http://192.168.0.24:8000/valid_ingredient_list/')
+    response = requests.get('http://192.168.0.18:8000/valid_ingredient_list/')
     assert response.status_code == 200
     assert response.headers["Content-Type"] == "application/json"
     response_body = response.json()
@@ -12,14 +13,15 @@ def test_get_valid_ingredient_api_response() -> None:
     assert response_body["Ingredients"][9] == ['onions']
 
 def test_check_recipe_result_api_response() -> None:
-    response = requests.get('http://192.168.0.24:8000/search_recipes_by_ingredients/gravy,tapenade/100')
+    response = requests.get('http://192.168.0.18:8000/search_recipes_by_ingredients/parsley,bacon/10')
     assert response.status_code == 200
     assert response.headers["Content-Type"] == "application/json"
     response_body = response.json()
-    assert response_body['Recipes'][0]['Title']  == 'Boned roast leg of lamb'
-    assert response_body['Recipes'][0]['Ingredients'] == 'garlic, potatoes, honey, feta cheese, anchovies, beans, leg of lamb, gravy, tapenade'
-    assert response_body['Recipes'][0]['URL'] == 'https://www.google.co.uk/search?q=Boned roast leg of lamb'
-    assert response_body['Recipes'][0]['Picture_URL'] == 'http://res.cloudinary.com/uktv/image/upload/v1420626176/mfda7erqx2ll8pagddlh.jpg'
+    assert response_body['Recipes'][1]['Title']  == 'Savoy cabbage and smoked bacon soup'
+    assert response_body['Recipes'][1]['Ingredients'] == '<ingredient>1 tbsp olive oil</ingredient><ingredient>1 clove garlic, chopped</ingredient><ingredient>2 baby leeks, finely chopped</ingredient><ingredient>3 rashers bacon, finely chopped</ingredient><ingredient>½ Savoy cabbage, shredded</ingredient><ingredient>500ml/18fl oz hot chicken stock</ingredient><ingredient>2 tbsp fresh parsley, finely chopped</ingredient><ingredient>2 tbsp olive oil</ingredient><ingredient>salt and freshly ground black pepper</ingredient>'
+    assert response_body['Recipes'][1]['Servings'] == 'Serves 1'
+    assert response_body['Recipes'][1]['Method'] == '<step1>To make the soup, heat the olive oil in a saucepan over a medium heat. Add the garlic and leeks and fry 2-3 minutes.</step1><step2>Add the chopped bacon and continue to fry for another 3-4 minutes to brown the bacon.</step2><step3>Add the cabbage and fry, stirring regularly, for 2-3 minutes, until soft.</step3><step4>Add the chicken stock and bring to the boil, then reduce the heat and simmer for 10 minutes.</step4><step5>Use a hand-blender to liquidise the soup and season, to taste, with salt and freshly ground black pepper.</step5><step6>To make the parsley oil, place the chopped parsley and the olive oil in a bowl and season, to taste, with salt and freshly ground black pepper.</step6><step7>To serve, pour the soup into a warm bowl and drizzle with the parsley oil.</step7>'
+    assert response_body['Recipes'][1]['Picture_URL'] == ''
 
 
 
